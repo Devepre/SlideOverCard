b7dc36f06daed1e1a42122466248287c1bb1b861
Serhii Kyrylenko <bicycle2life@gmail.com>
2020-03-22 12:35:58 +0200

    Initial Commit

---
+++ SlideOverCard/SlideOverCard.h
@@ -0,0 +1,19 @@
+//
+//  SlideOverCard.h
+//  SlideOverCard
+//
+//  Created by Serhii Kyrylenko on 22.03.2020.
+//  Copyright © 2020 Opensource. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SlideOverCard.
+FOUNDATION_EXPORT double SlideOverCardVersionNumber;
+
+//! Project version string for SlideOverCard.
+FOUNDATION_EXPORT const unsigned char SlideOverCardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideOverCard/PublicHeader.h>
+
+
